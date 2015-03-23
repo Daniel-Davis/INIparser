9e3df0d65928ebd359992a3728adeffcfda3d310
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-22 18:38:18 -0700

    Update INIparser.js

--- INIparser.js
+++ INIparser.js
@@ -1,6 +1,6 @@
 // Daniel Davis, INIparser, prints out parsed items //
 
-function parseINI(string) {
+function parseINI(string) { // function parseINI
   // Start with an object to hold the top-level fields
   var currentSection = {name: null, fields: []};
   var categories = [currentSection];
@@ -17,8 +17,9 @@ function parseINI(string) {
                                   value: match[2]});
     } else {
       throw new Error("Line '" + line + "' is invalid.");
-    }
-  });
-
+    } // end of else
+  }); // end of string.split
   return categories;
-}
+  JSON.stringify(categories);
+  print(catergories);
+} // end of function
